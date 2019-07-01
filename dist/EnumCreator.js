3c5b942dab4b60d959ab89d9064530b03cbb4034
Ken <kyrolites@gmail.com>
2019-07-01 12:41:10 +0800

    comment stuff and tiny fix

--- dist/EnumCreator.js
+++ dist/EnumCreator.js
@@ -1,6 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var EnumCreator = (function () {
+var EnumCreator = /** @class */ (function () {
+    /**
+     * @constructor
+     * @param {*} args - Ability to set any arguments to be parsed automatically. For options to work you should pass them BEFORE enum declaration
+     */
     function EnumCreator() {
         var args = [];
         for (var _i = 0; _i < arguments.length; _i++) {
@@ -15,8 +19,8 @@ var EnumCreator = (function () {
         this.strict = true;
         this.visibleEnums = true;
         this.editableEnums = false;
-        this.allowDuplicateValues = false;
-        this._allowedOptions = ["strict", "allowDuplicateValues", "visibleEnums", "editableEnums"];
+        // private allowDuplicateValues: boolean = false; - removed until I figure out what i broke with these options
+        this._allowedOptions = ["strict", "visibleEnums", "editableEnums"];
         this._keys = [];
         this._values = [];
         for (var _a = 0, args_1 = args; _a < args_1.length; _a++) {
@@ -31,18 +35,46 @@ var EnumCreator = (function () {
                 default: throw new TypeError("Invalid argument detected: \"" + typeof arg + "\" was provided. Expected STRING, ARRAY, OBJECT.");
             }
         }
+        // @ts-ignore
         return this.enums;
     }
+    /**
+     * Serves literally no purpose, use .set() instead.
+     * @param {string} key
+     * @param {string|boolean} value
+     * @public
+     * @return {set}
+     */
     EnumCreator.prototype.update = function (key, value) {
         console.log("WARN :: Update is now deprecated (I feel like a big boy saying this) use 'set' instead.");
         return this.set(key, value);
     };
+    /**
+     * Manually define a key, value (or just key) enum.
+     * @param {string} key
+     * @param {string|boolean} value
+     * @public
+     * @return {_define}
+     */
     EnumCreator.prototype.set = function (key, value) {
         return this._define([key, value]);
     };
+    /**
+     * Check to see if an enum exists using the key provided.
+     * @param {string} key - Not case sensitive.
+     * @public
+     * @return {boolean}
+     */
     EnumCreator.prototype.has = function (key) {
-        return (this._keys.includes(key));
+        // @ts-ignore
+        return (this._keys.includes(key.toUpperCase()));
     };
+    /**
+     * Takes an object from constructor's arguments and parses it as "options" for the enums.
+     * @param {object} options
+     * @private
+     * @return {string}
+     */
     EnumCreator.prototype._handleOptions = function (options) {
         if (this._handledOptions) {
             console.log("WARN :: Attempt to use more options, remove the extra OBJECT in your Enum declaration. (Ignoring)");
@@ -51,8 +83,10 @@ var EnumCreator = (function () {
         this._handledOptions = true;
         var optionNames = Object.getOwnPropertyNames(options);
         var _loop_1 = function (opt) {
+            // @ts-ignore
             var sameValueType = this_1._allowedOptions.includes(opt) && typeof this_1[opt] === typeof options[opt];
             var err = new TypeError("Invalid option. Please check README for valid options.");
+            // @ts-ignore
             if (!this_1._allowedOptions.includes(opt))
                 throw err;
             if (!sameValueType) {
@@ -71,20 +105,37 @@ var EnumCreator = (function () {
         }
     };
     ;
+    /**
+     * Used to create a randomized string
+     * @static
+     * @return {string}
+     */
     EnumCreator._randomString = function (length) {
         var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; // 0123456789
         for (var i = 0; i < length; i++)
             result += characters.charAt(Math.floor(Math.random() * characters.length));
         return result;
     };
+    /**
+     * The internal function used to check that the random string it is creating doesn't exist already
+     * @private
+     * @return {string}
+     */
     EnumCreator.prototype._createValue = function () {
         var value = EnumCreator._randomString(4);
+        // @ts-ignore
         while (this._values.includes(value)) {
             value = EnumCreator._randomString(4);
         }
         return value;
     };
+    /**
+     * The internal function used to define arrays of key or key/value enums
+     * @param {string|array} arg - Represents a key(string) or an array of multiple key value arrays
+     * @private
+     * @return {void}
+     */
     EnumCreator.prototype._definePotentialMultiple = function (arg) {
         var _this = this;
         if (typeof arg === "object" && !Array.isArray(arg))
@@ -136,6 +187,12 @@ var EnumCreator = (function () {
             }
         }
     };
+    /**
+     * The internal function used to define the enums to EnumCreator.enums
+     * @param {string|array} arg - Represents a key(string) or key and value(array{2})
+     * @private
+     * @return {string|number}
+     */
     EnumCreator.prototype._define = function (arg) {
         if (typeof arg === "object" && !Array.isArray(arg))
             throw new TypeError("You cannot define objects as an enum. (Yet?)");
@@ -145,39 +202,56 @@ var EnumCreator = (function () {
                 key = arg;
                 value = this._createValue();
                 break;
-            case 'object':
+            case 'object': // "array"
                 key = arg[0], value = arg[1];
                 if (value === undefined || value === null)
                     value = this._createValue();
                 break;
         }
+        // @ts-ignore
+        if (!["string", "number"].includes(typeof value))
+            throw new TypeError("\"" + value + "\" is type " + (typeof value).toUpperCase() + ". Expected type STRING or NUMBER.");
         if (typeof key !== "string")
             throw new TypeError("\"" + key + "\" is type " + (typeof key).toUpperCase() + ". Enum names must be type STRING");
+        // @ts-ignore
         key = key.toUpperCase();
         key = key
+            // remove unneeded characters for enums
             .replace(/[^A-z\s\d][\\^]?|[ +]|[0-9]/g, '_')
+            // replace any repeated `_` with a single `_`
             .replace(/_+/g, '_');
-        var keyExists = this._keys.includes(key), valueExists = this._values.includes(value);
+        // @ts-ignore
+        var keyExists = this._keys.includes(key), 
+        // @ts-ignore
+        valueExists = this._values.includes(value);
+        // @ts-ignore
         var existingType = keyExists ? key : value;
         if (this.strict && (keyExists || valueExists))
             throw new Error("There is already an enum with the " + (keyExists ? "name" : "value") + ": \"" + existingType + "\".");
+        // @ts-ignore
         this._keys.push(key);
+        // @ts-ignore
         this._values.push(value);
         var attributes = {
+            // @ts-ignore
             get: function () { return value; },
             enumerable: this.visibleEnums
         };
         if (this.editableEnums) {
+            // @ts-ignore
             attributes.set = function (newValue) {
                 value = newValue;
             };
         }
         else if (!this.editableEnums && this.strict) {
+            // @ts-ignore
             attributes.set = function () {
                 throw new Error("Tried editing a static enum whilst in strict mode. Check README.md.");
             };
         }
+        // @ts-ignore
         Object.defineProperty(this.enums, key, attributes);
+        // @ts-ignore
         return value;
     };
     return EnumCreator;
--- src/EnumCreator.ts
+++ src/EnumCreator.ts
@@ -1,5 +1,10 @@
 
+
+
 export class EnumCreator {
+
+
+
 
     private _keys: string[];
     private _values: string[];
@@ -14,10 +19,17 @@ export class EnumCreator {
     private strict: boolean = true;
     private visibleEnums: boolean = true;
     private editableEnums: boolean = false;
-    private allowDuplicateValues: boolean = false;
+    // private allowDuplicateValues: boolean = false; - removed until I figure out what i broke with these options
 
-    private _allowedOptions: string[] = ["strict", "allowDuplicateValues", "visibleEnums", "editableEnums"];
+    private _allowedOptions: string[] = ["strict", "visibleEnums", "editableEnums"];
 
+
+
+
+    /**
+     * @constructor
+     * @param {*} args - Ability to set any arguments to be parsed automatically. For options to work you should pass them BEFORE enum declaration
+     */
     constructor(...args : any) {
 
         this._keys = [];
@@ -35,20 +47,58 @@ export class EnumCreator {
         return this.enums;
     }
 
-    public update (key : string, value : any) : any  {
+
+
+
+    /**
+     * Serves literally no purpose, use .set() instead.
+     * @param {string} key
+     * @param {string|boolean} value
+     * @public
+     * @return {set}
+     */
+    public update (key : string, value : (string | boolean)) : any {
         console.log("WARN :: Update is now deprecated (I feel like a big boy saying this) use 'set' instead.");
         return this.set(key, value);
     }
 
-    public set (key : string, value : any) : any {
+
+
+
+    /**
+     * Manually define a key, value (or just key) enum.
+     * @param {string} key
+     * @param {string|boolean} value
+     * @public
+     * @return {_define}
+     */
+    public set (key : string, value : (string | boolean)) : any {
         return this._define([key, value]);
     }
 
+
+
+
+    /**
+     * Check to see if an enum exists using the key provided.
+     * @param {string} key - Not case sensitive.
+     * @public
+     * @return {boolean}
+     */
     public has (key : string) : boolean {
         // @ts-ignore
-        return (this._keys.includes(key));
+        return (this._keys.includes(key.toUpperCase()));
     }
 
+
+
+
+    /**
+     * Takes an object from constructor's arguments and parses it as "options" for the enums.
+     * @param {object} options
+     * @private
+     * @return {string}
+     */
     private _handleOptions (options : any) : void {
         if (this._handledOptions) {
             console.log("WARN :: Attempt to use more options, remove the extra OBJECT in your Enum declaration. (Ignoring)");
@@ -80,6 +130,14 @@ export class EnumCreator {
 
     };
 
+
+
+
+    /**
+     * Used to create a randomized string
+     * @static
+     * @return {string}
+     */
     static _randomString (length: number) : string {
         let result = '';
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; // 0123456789
@@ -87,6 +145,14 @@ export class EnumCreator {
         return result;
     }
 
+
+
+
+    /**
+     * The internal function used to check that the random string it is creating doesn't exist already
+     * @private
+     * @return {string}
+     */
     private _createValue () : string {
         let value = EnumCreator._randomString(4);
 
@@ -98,6 +164,16 @@ export class EnumCreator {
         return value;
     }
 
+
+
+
+
+    /**
+     * The internal function used to define arrays of key or key/value enums
+     * @param {string|array} arg - Represents a key(string) or an array of multiple key value arrays
+     * @private
+     * @return {void}
+     */
     private _definePotentialMultiple (arg : object) : void {
         if (typeof arg === "object" && !Array.isArray(arg)) throw new TypeError("You cannot define objects as an enum. (Yet?)");
 
@@ -148,6 +224,15 @@ export class EnumCreator {
         }
     }
 
+
+
+
+    /**
+     * The internal function used to define the enums to EnumCreator.enums
+     * @param {string|array} arg - Represents a key(string) or key and value(array{2})
+     * @private
+     * @return {string|number}
+     */
     private _define (arg : (string | object)) : any {
         if (typeof arg === "object" && !Array.isArray(arg)) throw new TypeError("You cannot define objects as an enum. (Yet?)");
 
@@ -165,6 +250,8 @@ export class EnumCreator {
                 break;
         }
 
+        // @ts-ignore
+        if (!["string", "number"].includes(typeof value)) throw new TypeError(`"${value}" is type ${(typeof value).toUpperCase()}. Expected type STRING or NUMBER.`);
         if (typeof key !== "string") throw new TypeError(`"${key}" is type ${(typeof key).toUpperCase()}. Enum names must be type STRING`);
         // @ts-ignore
         key = key.toUpperCase();
@@ -214,5 +301,8 @@ export class EnumCreator {
         // @ts-ignore
         return value;
     }
+
+
+
 
 }
